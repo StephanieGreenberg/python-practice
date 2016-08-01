e99d346c9a91deeca7cdec438b1b3d25084a3410
Greenberg <XBBNGLG@ams.bnymellon.net>
2016-08-01 14:12:46 -0400

    finished exercise 02

--- 01-characterinput.py
+++ 01-characterinput.py
@@ -1,12 +1,11 @@
 import datetime
-import sys
 
 now = datetime.datetime.now()
 name = raw_input("Please enter your name: ")
 age = int(raw_input("Please enter your age: "))
 
 #function calculates which year the user will turn 100
-def calculate_100_year(age):
+def calculate_100_year(age, now):
 	return now.year + (100 - age)
 
-print "%s, you will be 100 years old in %d. Happy early birthday!" % (name, calculate_100_year(age))
+print "%s, you will be 100 years old in %d. Happy early birthday!" % (name, calculate_100_year(age, now))
---
+++ 02-oddoreven.py
@@ -0,0 +1,6 @@
+num = int(raw_input("Please enter a number: "))
+
+if num % 2 == 0:
+	print "Your number is even."
+else:
+	print "Your number is odd."
